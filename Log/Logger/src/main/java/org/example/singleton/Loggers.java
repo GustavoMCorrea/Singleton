2a2ace6b83e2a73fddd507513ce8f5f157bdf599
GustavoMCorrea <gustavomotacorrea@gmail.com>
2024-03-18 18:38:56 -0300

    Diagrama de Classe

--- Log/Logger/src/main/java/org/example/singleton/Loggers.java
+++ Log/Logger/src/main/java/org/example/singleton/Loggers.java
@@ -6,7 +6,34 @@ public class Loggers {
     public static Loggers getInstance() {
         return instance;
     }
+    private String usario;
+    private String login;
+    private String logout;
     private String message;
+
+    public String getUsario() {
+        return usario;
+    }
+
+    public void setUsario(String usario) {
+        this.usario = usario;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getLogout() {
+        return logout;
+    }
+
+    public void setLogout(String logout) {
+        this.logout = logout;
+    }
 
     public String getMessage() {
         return message;
